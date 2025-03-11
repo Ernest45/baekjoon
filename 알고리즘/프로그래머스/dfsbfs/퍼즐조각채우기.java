07924e00dea15d9bf280c53a9b9b883564c47422
Ernest45 <gkgkswns71@gmail.com>
2025-03-11 23:39:17 +0900

    퍼즐조각 채우기

--- 알고리즘/프로그래머스/dfsbfs/퍼즐조각채우기.java
+++ 알고리즘/프로그래머스/dfsbfs/퍼즐조각채우기.java
@@ -1,2 +1,33 @@
-package 알고리즘.프로그래머스.dfsbfs;public class 퍼즐조각채우기 {
+package 알고리즘.프로그래머스.dfsbfs;
+
+public class 퍼즐조각채우기 {
+    // 보드에 맞게 테이블을 활용해 채우기
+    // 채울 때 table들을 활용해야 하는데, 회전할 수 있다
+    // 하나씩 쓸 수 있다.
+    // 채울 때  여러개의 테이블을 쓰거나 딱 맞지 않게 쓸 수 없다 (두개를 조합 시ㅣㅋ기도 불가능)
+    public static void main(String[] args) {
+
+        int game_board[][] = new int[][]{
+                {1, 1, 0, 0, 1, 0},
+                {0, 0, 1, 0, 1, 0},
+                {0, 1, 1, 0, 0, 1},
+                {1, 1, 0, 1, 1, 1},
+                {1, 0, 0, 0, 1, 0},
+                {0, 1, 1, 1, 0, 0}};
+
+        int table[][] = new int[][]{
+                {1, 0, 0, 1, 1, 0},
+                {1, 0, 1, 0, 1, 0},
+                {0, 1, 1, 0, 1, 1},
+                {0, 0, 1, 0, 0, 0},
+                {1, 1, 0, 1, 1, 0},
+                {0, 1, 0, 0, 0, 0}};
+    }
+
+
+    public int solution(int[][] game_board, int[][] table) {
+        int answer = -1;
+        return answer;
+    }
+
 }
