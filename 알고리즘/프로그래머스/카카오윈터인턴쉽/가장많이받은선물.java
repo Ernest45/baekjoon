948e09beeefcdbadc0c7645f9978c573ff5a5f47
Ernest45 <gkgkswns71@gmail.com>
2025-03-13 02:28:51 +0900

    카카오인턴쉽 가장 많이 받은 선물 구현

--- 알고리즘/프로그래머스/카카오윈터인턴쉽/가장많이받은선물.java
+++ 알고리즘/프로그래머스/카카오윈터인턴쉽/가장많이받은선물.java
@@ -1,5 +1,7 @@
 package 알고리즘.프로그래머스.카카오윈터인턴쉽;
 
+import java.util.HashMap;
+
 public class 가장많이받은선물 {
 
     // 선물하기 기능
@@ -22,5 +24,64 @@ public class 가장많이받은선물 {
                 "frodo muzi",
                 "frodo ryan",
                 "neo muzi"};
+
+        가장많이받은선물 main = new 가장많이받은선물();
+        main.solution(friend, gifts);
+    }
+
+    // 1. 각 사람을 담을 공간을 생성 후 받은 선물을 기록 + 받고 줄 때의 지수까지 기록
+    // 2. 받은 선물들을 비교해 돌면서 다음 달 선물 받을 기록을 따로 담기
+    // 3. 가장 큰 선물을 반환
+
+
+    public int solution(String[] friends, String[] gifts) {
+        int size = friends.length;
+        int answer = Integer.MIN_VALUE;
+        int[][] giftList = new int[size][size];
+        int answerList[] = new int[size];
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < size; i++) {
+            map.put(friends[i], i);
+        }
+        for (int i = 0; i < gifts.length; i++) {
+
+            String[] now = gifts[i].split(" ");
+
+            int to = map.get(now[0]);
+            int from = map.get(now[1]);
+
+            giftList[to][from]++;
+            answerList[to]++;
+            answerList[from]--;
+//            giftList[to][to]++;
+//            giftList[from][from]--;
+
+        }
+        int[] nextGift = new int[size];
+
+        for (int i = 0; i <size ; i++) {
+            for (int j = i + 1; j < size; j++) {
+
+                if (giftList[i][j] < giftList[j][i]) {
+                    nextGift[j]++;
+
+                } else if (giftList[i][j] > giftList[j][i]) {
+                    nextGift[i]++;
+                } else
+                    if(answerList[j] < answerList[i]){
+                        nextGift[i]++;
+
+                    } else if (answerList[j] > answerList[i]) {
+                        nextGift[j]++;
+                    }
+            }
+        }
+
+        for (int i : nextGift) {
+            answer = Math.max(i, answer);
+        }
+
+        return answer;
     }
 }
