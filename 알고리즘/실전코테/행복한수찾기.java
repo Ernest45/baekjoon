fc9a7fc943f76b0a80c806d5690f9e52165229a0
Ernest45 <gkgkswns71@gmail.com>
2025-04-28 00:47:47 +0900

    행복한 수 찾기

--- 알고리즘/실전코테/행복한수찾기.java
+++ 알고리즘/실전코테/행복한수찾기.java
@@ -1,4 +1,46 @@
 package 알고리즘.실전코테;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+
 public class 행복한수찾기 {
-}
+
+    /**
+     * 행복 수
+     * 각 자리수의 제곱의 합으로 변환 시 언젠가 1에 도달할 수
+     * ex)  1. 13 = 1x1 + 3x3 = 1 + 9 -> 10
+     *      2. 1x1 + 0x0 = 1 행복 수 도달
+     *      범위 1~9999, 범위가 주어졌을 때 해당 범위 내 행복 수를 모두 찾아 return
+     *      입력 예시) 1
+     *              9
+     *
+     *      출력 예시) 1 ~ 9 범위의 행복 수는 2개이고 총합은 8입니다.
+     *
+     */
+
+
+    public static void main(String[] args) throws IOException {
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        int count = 0;
+//        int sum = 0;
+//        int start = Integer.parseInt(br.readLine());
+//        int end = Integer.parseInt(br.readLine());
+//
+//        if(start>0 && end>0) {}
+//
+//        while (start > end) {
+//
+//            String now = String.valueOf(start);
+//
+//            for (char ch : now.toCharArray()) {
+//
+//
+//            }
+
+        }
+
+    }
